17780b41c8a2b3ec268903fd844c9af0a19cf52b
Nguyen Anh Khoa <akhoa0805@gmail.com>
2025-02-28 17:23:41 +0700

    up

--- data/user-data.js
+++ data/user-data.js
@@ -1,13 +1,28 @@
 export const userData = {
-  githubUser: 'said7388',
-  devUsername: "said7388",
-  github: 'https://github.com/said7388',
-  facebook: 'https://www.facebook.com/abusaid.riyaz/',
-  linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
-  twitter: 'https://twitter.com/said7388',
-  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  name: "Nguyễn Anh Khoa",
+  githubUser: "ankoa",
+  devUsername: "ankoa",
+  github: "https://github.com/ankoa",
+  facebook: "https://www.facebook.com/profile.php?id=100009939158651",
+  linkedIn:
+    "https://www.linkedin.com/in/nguy%E1%BB%85n-anh-khoa-undefined-805a6a32a/",
+  twitter: "https://twitter.com/said7388",
+  stackOverflow: "https://stackoverflow.com/users/16840768/abu-said",
   leetcode: "https://leetcode.com/said3812/",
-  resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
-  skills: ['React', 'NextJS', 'Redux', 'Express', 'NestJS', 'MySql', 'MongoDB', 'Postgres', 'Docker', 'AWS'],
-  timezone: '+6'
+  resume:
+    "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",
+  skills: [
+    "React",
+    "Redux",
+    "MySql",
+    "MongoDB",
+    "Postgres",
+    "Docker",
+    "AWS",
+    "C#",
+    "Java",
+    "Bootstrap",
+    "Odoo",
+  ],
+  timezone: "+7",
 };
